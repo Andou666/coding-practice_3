cf64778b443070a3255effb7068396a53f04ee2a
Andou666 <androncoron@gmail.com>
2022-05-07 15:21:20 +0900

    commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(function () {
+
+    //ページ内スクロール
+    $('a[href^="#"]').on('click', function () {
+      const speed = 300;
+      const href = $(this).attr("href");
+      const target = $(href == "#" || href == "" ? 'html' : href);
+      const position = target.offset().top;
+      $("html, body").animate({
+        scrollTop: position
+      }, speed, "swing");
+      return false;
+    });
+
+    //ページトップへ戻る
+    const $pageTop = $('.page-top');
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 300) {
+        $pageTop.fadeIn();
+      } else {
+        $pageTop.fadeOut();
+      }
+    });
+    $pageTop.on('click', function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 300);
+      return false;
+    });
+
+
+});
